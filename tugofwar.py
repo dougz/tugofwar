252eded0dea876b18df3a0438eb14043938e61b8
Doug Zongker <dougz@isotropic.org>
2019-11-03 19:22:55 -0800

    change to TCP port

--- tugofwar.py
+++ tugofwar.py
@@ -257,8 +257,8 @@ def main():
   parser.add_argument("-c", "--cookie_secret",
                       default="snellen2020",
                       help="Secret used to create session cookies.")
-  parser.add_argument("--socket_path", default="/tmp/tugofwar",
-                      help="Socket for requests from frontend.")
+  parser.add_argument("--listen_port", type=int, default=2003,
+                      help="Port requests from frontend.")
   parser.add_argument("--wait_url", default="tugwait",
                       help="Path for wait requests from frontend.")
   parser.add_argument("--main_server_port", type=int, default=2020,
